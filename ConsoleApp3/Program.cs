a71877ddcf7f0db8d70fc32bb75d695bb81d080a
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2024-12-10 11:19:37 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        /// <summary>
+        /// 10. Удаление всех четных элементов из массива.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] mas = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //    mas = mas.Where(x => x % 2 != 0).ToArray();
+            //    foreach (int x in mas)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+
+            //int sum = mas.Sum();
+            //Console.WriteLine(sum);
+            Console.Read();
+        }
+    }
+}

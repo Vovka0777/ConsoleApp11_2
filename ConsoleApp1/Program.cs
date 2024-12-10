87dceda8e76d0f6358b9aa413fa9c4b8766f662f
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2024-12-10 11:18:07 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// 6. Подсчет количества вхождений определенного числа в массив.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 1, 9, 5, 3, 6, 3 };
+            //int count = 0;
+            //foreach (int x in array)
+            //{
+            //   if (x == 3)
+            //   {
+            //    count++;
+            //   }
+            //}
+            //Console.WriteLine(count);
+
+            int count = array.Count(x=>x==3);
+            Console.WriteLine(count);
+            Console.Read();
+        }
+    }
+}
